80b6f2806323e74bd5426fd0a3343d3ec9d12cb1
antonelli182 <32582969+antonelli182@users.noreply.github.com>
2020-07-01 12:32:33 -0700

    update start form

--- src/components/StartForm.js
+++ src/components/StartForm.js
@@ -1,9 +1,31 @@
 import React from 'react';
 
+function encode(data) {
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&')
+}
+
+const handleSubmit = (e) => {
+  e.preventDefault()
+  const form = e.target
+  fetch('/', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    body: encode({
+      'form-name': form.getAttribute('name'),
+      ...state,
+    }),
+  })
+    .then(() => navigate(form.getAttribute('action')))
+    .catch((error) => alert(error))
+}
+
+
 export default class StartForm extends React.Component {
     render() {
         return (
-            <form name="startForm" method="POST" data-netlify-honeypot="bot-field" data-netlify="true" id="start-form" className="start-form">
+            <form name="startForm" method="POST" data-netlify-honeypot="bot-field" data-netlify="true" id="start-form" className="start-form" action="/signup/">
               <input type="hidden" name="form-name" value="startForm" />
               <div className="screen-reader-text">
                 <label>Don't fill this out if you're human: <input name="bot-field" /></label>
