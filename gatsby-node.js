a539881b5090caae2bb3a992d6c0d59356902ae2
Nazar Vovk <nazarvovk.work@gmail.com>
2020-07-10 13:34:21 +0300

    Landing Start Form

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,3 +3,15 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
+
+exports.onCreateWebpackConfig = ({ stage, actions }) => {
+  if (stage.startsWith('develop')) {
+    actions.setWebpackConfig({
+      resolve: {
+        alias: {
+          'react-dom': '@hot-loader/react-dom',
+        },
+      },
+    });
+  }
+};
--- src/components/StartForm.js
+++ src/components/StartForm.js
@@ -1,44 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { navigate } from 'gatsby';
 
-function encode(data) {
-  return Object.keys(data)
-    .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
-    .join('&')
-}
+const StartForm = () => {
+  const [email, setEmail] = useState('');
 
-const handleSubmit = (e) => {
-  e.preventDefault()
-  const form = e.target
-  fetch('/', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-    body: encode({
-      'form-name': form.getAttribute('name'),
-      ...state,
-    }),
-  })
-    .then(() => navigate(form.getAttribute('action', ...state)))
-    .catch((error) => alert(error))
-}
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append('email', email);
 
-export default class StartForm extends React.Component {
-    render() {
-        return (
-            <form name="startForm" method="POST" data-netlify-honeypot="bot-field" data-netlify="true" id="start-form" className="start-form" action="/signup/">
-              <input type="hidden" name="form-name" value="startForm" />
-              <div className="screen-reader-text">
-                <label>Don't fill this out if you're human: <input name="bot-field" /></label>
-              </div>
-              <div className="form-row">
-                <label>
-                  <span className="screen-reader-text">Email Comercial</span>
-                  <input className="lead-email" type="email" required name="email" placeholder="Seu email comercial"/>
-                </label>
-              </div>
-              <input type="hidden" name="form-name" value="startForm" />
-              <button className="button" type="submit">Começar Agora</button>
-            </form>
-        );
-    }
-}
+    const requestOptions = {
+      method: 'POST',
+      body: formData,
+      redirect: 'follow',
+    };
 
+    fetch(
+      'https://hooks.zapier.com/hooks/catch/142211/ozo0et4/',
+      requestOptions
+    )
+      .then(() => navigate('signup', { state: { email } }))
+      .catch((error) => console.log('error', error));
+  };
+
+  return (
+    <form
+      name='startForm'
+      data-netlify-honeypot='bot-field'
+      data-netlify='true'
+      id='start-form'
+      className='start-form'
+      onSubmit={handleSubmit}
+    >
+      <input type='hidden' name='form-name' value='startForm' />
+      <div className='screen-reader-text'>
+        <label>
+          Don&apos;t fill this out if you&apos;re human: <input name='bot-field' />
+        </label>
+      </div>
+      <div className='form-row'>
+        <label>
+          <span className='screen-reader-text'>Email Comercial</span>
+          <input
+            className='lead-email'
+            type='email'
+            required
+            name='email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder='Seu email comercial'
+          />
+        </label>
+      </div>
+      <input type='hidden' name='form-name' value='startForm' />
+      <button className='button' type='submit'>
+        Começar Agora
+      </button>
+    </form>
+  );
+};
+
+export default StartForm;
