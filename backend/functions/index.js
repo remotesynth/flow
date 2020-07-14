c2ead6da6e0bb3aca42582eef844849c9ede0303
antonelli182 <32582969+antonelli182@users.noreply.github.com>
2020-07-14 07:50:14 -0700

    update dealUpdate cloud function

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -6,17 +6,22 @@ admin.initializeApp(functions.config().firebase);
 
 exports.updateDealStatus = functions.https.onRequest((req, res) => {
 
-  console.log(req.body);
-  // Update the Deal in Cloud Firestore using the Firebase Admin SDK
- // const updateDeal = admin.firestore().collection('Deals')
- // .doc(req.hs_deal_id)
- // .update({
- //   deal_stage: "",
- //   update_date: "" 
- //   });
-  
-  // Send back a message that we've succesfully udpdated the deal 
- // res.json({result: `Message with ID: added.`});
-
+ // Update the Deal in Cloud Firestore using the Firebase Admin SDK
+const body = req.body;
+const updateDeal = admin.firestore().collection('Deals')
+  .doc(body.hs_deal_id)
+  .update({
+    deal_stage: body.deal_stage,
+    update_date: body.update_date,
+    deal_value: body.deal_value,
+    deal_name: body.deal_name
+    })
+    .then( function(){
+      res.send("Deal Updated Success")
+    })
+    .catch(error => { 
+        console.log(error)
+        res.status(500).send(error)
+      })
 });
 
